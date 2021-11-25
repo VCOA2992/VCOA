570443083595083b3eb921a7fb7b9a45f9fbb67b
Binamra Lamsal <61900781+binamracode@users.noreply.github.com>
2021-11-25 07:12:56 +0545

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ const token = config.BOT_TOKEN;
 
 const port = process.env.PORT || 443;
 const host = '0.0.0.0';
-const externalUrl = process.env.CUSTOM_ENV_VARIABLE || 'https://my-app.herokuapp.com',
+const externalUrl = process.env.CUSTOM_ENV_VARIABLE || 'https://my-app.herokuapp.com';
 
 const bot = new TelegramBot(token, { polling: true, webHook: {port: port, host: host }});
 bot.setWebHook(externalUrl + ':443/bot' + token);
