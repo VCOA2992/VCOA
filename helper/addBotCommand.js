105479e667f07d08f03783ca0eb3d628d341754e
babit-pandey <binamraprogrammer@gmail.com>
2021-12-16 22:16:25 +0545

    Set commands

--- helper/addBotCommand.js
+++ helper/addBotCommand.js
@@ -5,25 +5,21 @@ const allCommands = [];
 allCommands.push({
   command: "/add",
   description: "To add files of channel in group to be searchable",
-  options: { scope: { type: "all_chat_administrators" } },
 });
 
 allCommands.push({
   command: "/del",
   description: "To delete files of channel in group to be searchable",
-  options: { scope: { type: "all_chat_administrators" } },
 });
 
 allCommands.push({
   command: "/delall",
   description: "To delete all files added in a group",
-  options: { scope: { type: "all_chat_administrators" } },
 });
 
 allCommands.push({
   command: "/filterstats",
   description: "To get list of all channels that are added to a group",
-  options: { scope: { type: "all_chat_administrators" } },
 });
 
-bot.setMyCommands(allCommands);
+bot.setMyCommands(allCommands, { scope: { type: "all_group_chats" } });
