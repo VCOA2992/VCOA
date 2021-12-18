2165ff31c6f37a96bfe1ddd85654c2dd1be8bf9a
Binamra Lamsal <61900781+binamracode@users.noreply.github.com>
2021-12-18 17:44:30 +0545

    Update callbacks.js

--- commands/callbacks.js
+++ commands/callbacks.js
@@ -6,15 +6,17 @@ import bot from "../config/bot.js";
 import { AUTHORIZED_USERS } from "../config/config.js";
 import pagination from "../controllers/callbacks/pagination.js";
 
-const authorized_users = AUTHORIZED_USERS;
-
 bot.on("callback_query", async (query) => {
   const userWhoClicked = query.from.id;
   const userWhoRequested = query.message.reply_to_message.from.id;
 
   if (
     userWhoClicked === userWhoRequested ||
-    authorized_users.includes(userWhoClicked)
+    AUTHORIZED_USERS.includes(userWhoClicked)
   )
     pagination(query);
+  else{
+    await bot.answerCallbackQuery(query.id);
+    console.log(`You are not in authorized users.\nUsers: ${AUTHORIZED_USERS}`);
+  }
 });
