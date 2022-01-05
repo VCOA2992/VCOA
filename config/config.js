f6198a4d14d97ad37c8b147ba44ad967c77c2aa9
Binamra Lamsal <61900781+binamracode@users.noreply.github.com>
2022-01-05 11:38:56 +0545

    Update config.js

--- config/config.js
+++ config/config.js
@@ -7,7 +7,7 @@ import "dotenv/config.js";
 export const AUTHORIZED_USERS = process.env.AUTHORIZED_USERS.split(" ");
 export const APP_ID = parseInt(process.env.APP_ID);
 export const API_HASH = process.env.API_HASH;
-export const STRING_SESSION = process.env.STRING_SESSION;
+export const STRING_SESSION = String(process.env.STRING_SESSION);
 export const BOT_TOKEN = process.env.BOT_TOKEN;
 export const MONGODB_URI = process.env.MONGODB_URI;
 export const LOG_CHANNEL = process.env.LOG_CHANNEL;
