21c4d890e48fdbc8ad9c5a0feeedeb2c99cbac58
Binamra Lamsal <binamralamsal@2400-1A00-B111.ip6.wlink.com.np>
2022-04-09 20:05:12 +0545

    Fixed some issues related to postgres

--- config/sqlite.js
+++ config/sqlite.js
@@ -1,8 +1,30 @@
 import { Sequelize } from "sequelize";
 import { DATABASE_URL } from "../config/config.js";
+import pg from "pg";
+
+const pool = new pg.Pool();
+
+pool.on("error", (err, client) => {
+  console.error("Unexpected error on idle client", err);
+  process.exit(-1);
+});
+
+pool.on("connect", (err, client) => {
+  if (err) console.error(err);
+  console.log(client);
+  console.log("Successfully connected to postgres.");
+});
 
 const sequelize = new Sequelize(DATABASE_URL, {
-  logging: false,
+  dialect: "postgres",
+  host: "localhost",
+  port: 5432,
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+  },
 });
 
 export default sequelize;
