fd8e1aba5160c53845c89bc0f85eea5b581fcdf7
Binamra Lamsal <binamralamsal@2400-1A00-B111.ip6.wlink.com.np>
2022-04-09 20:28:51 +0545

    Removed pool code from postgres

--- config/sqlite.js
+++ config/sqlite.js
@@ -1,19 +1,5 @@
 import { Sequelize } from "sequelize";
 import { DATABASE_URL } from "../config/config.js";
-import pg from "pg";
-
-const pool = new pg.Pool();
-
-pool.on("error", (err, client) => {
-  console.error("Unexpected error on idle client", err);
-  process.exit(-1);
-});
-
-pool.on("connect", (err, client) => {
-  if (err) console.error(err);
-  console.log(client);
-  console.log("Successfully connected to postgres.");
-});
 
 const sequelize = new Sequelize(DATABASE_URL, {
   dialect: "postgres",
