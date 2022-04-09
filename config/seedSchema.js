8fadaec4d0c554cbbfa8fc627dd275fbf25cc3ff
Binamra Lamsal <binamralamsal@2400-1A00-B111.ip6.wlink.com.np>
2022-04-09 21:17:18 +0545

    Added sample user in seedSchema

--- config/seedSchema.js
+++ config/seedSchema.js
@@ -1,8 +1,15 @@
 import "../models/ChatUser.js";
+import User from "../models/ChatUser.js";
 import database from "./sqlite.js";
 
 const main = async () => {
   await database.sync();
+
+  await User.create({
+    chatId: "991837880",
+    name: "Binamra Lamsal",
+    userName: "kodilearn",
+  });
 };
 
 main();
